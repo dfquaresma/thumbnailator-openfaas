7a04c8c7ac2953fb289dd0fa56963b8dc7c46ec2
dfquaresma <david.quaresma@ccc.ufcg.edu.br>
2019-05-08 18:36:50 -0300

    Remove gc data from function return

--- README.md
+++ README.md
@@ -19,6 +19,6 @@ $ faas-cli up -f thumbnailator-nogci.yml
 ```
 
 ### Result
-After deploying the OpenFaaS function `thumbnailator` will show up in the function list. You just have to hit invoke to run it. At each call, this will return the function's service time in nanoseconds and garbage collector data.
+After deploying the OpenFaaS function `thumbnailator` will show up in the function list. You just have to hit invoke to run it. At each call, this will return the function's service time in nanoseconds.
 
 Sample image used: https://user-images.githubusercontent.com/883386/53553708-ebb88a00-3b46-11e9-9ea8-73c6b7f9dfa1.jpg
--- thumbnailator/src/main/java/com/openfaas/function/Handler.java
+++ thumbnailator/src/main/java/com/openfaas/function/Handler.java
@@ -4,8 +4,6 @@ import com.openfaas.model.IHandler;
 import com.openfaas.model.IResponse;
 import com.openfaas.model.IRequest;
 import com.openfaas.model.Response;
-import java.lang.management.GarbageCollectorMXBean;
-import java.lang.management.ManagementFactory;
 import java.util.List;
 import java.lang.Error;
 import java.awt.geom.AffineTransform;
@@ -17,33 +15,14 @@ import javax.imageio.ImageIO;
 public class Handler implements com.openfaas.model.IHandler {
 
     public IResponse Handle(IRequest req) {
-        List<GarbageCollectorMXBean> gcs = ManagementFactory.getGarbageCollectorMXBeans();
-        GarbageCollectorMXBean scavenge = gcs.get(0);
-        GarbageCollectorMXBean markSweep = gcs.get(1);
-
-        long countBeforeScavenge = scavenge.getCollectionCount();
-        long timeBeforeScavenge = scavenge.getCollectionTime();
-        long countBeforeMarkSweep = markSweep.getCollectionCount();
-        long timeBeforeMarkSweep = markSweep.getCollectionTime();
         long before = System.nanoTime();
-
         String err = callFunction();
-
         long after = System.nanoTime();
-        long countAfterScavenge = scavenge.getCollectionCount();
-        long timeAfterScavenge = scavenge.getCollectionTime();
-        long countAfterMarkSweep = markSweep.getCollectionCount();
-        long timeAfterMarkSweep = markSweep.getCollectionTime();
-
+        
         String output = err + System.lineSeparator();
-        if (err.length() == 0) {
-            output = Long.toString(after - before) + "," + // Business Logic Time in Milliseconds
-                Long.toString(countAfterScavenge - countBeforeScavenge) + "," + // Scavenge Number of Collections
-                Long.toString(timeAfterScavenge - timeBeforeScavenge) + "," + // Scavenge Collections Time Spent in Milliseconds
-                Long.toString(countAfterMarkSweep - countBeforeMarkSweep) + "," + // MarkSweep Number of Collections
-                Long.toString(timeAfterMarkSweep - timeBeforeMarkSweep); // MarkSweep Collections Time Spent in Milliseconds
-        }
-
+        if (err.length() == 0) 
+            output = Long.toString(after - before); // Service Time in Nanoseconds
+        
         Response res = new Response();
         res.setBody(output);
         return res;
